a96b54ee259b64a971af43379e60d795036803a2
Brian Hirsh <hirshb@sas.upenn.edu>
2016-11-27 16:16:16 -0500

    wrote random block generating function, updated state to include a random list of integers (needed for this fxn)

--- tetris.hs
+++ tetris.hs
@@ -3,25 +3,27 @@
 module Tetris where
   
 import CodeWorld
-import Data.List
+import System.Random
 
-data State = S Double [[Coord]] [Coord] [Coord]
--- Double is previous time, [[Coord]] list of coord of all falling coord
+data State = S Double [[Coord]] [Coord] [Coord] [Integer]
+-- Double is previous time
+-- [[Coord]] list of coord of all falling coord
 -- [Coord] is currently falling list
 -- [Coord] is currently static blocks
+-- [Integer] is an infinite list of random numbers to choose blocks from
 
 --main :: IO ()
 --main = interactionOf initialState handleTime handleEvent drawState
 
-initialState = S 0 [[C 0 0]] [C 0 8] []
-mkInitialState blocks = S 0 blocks [C 0 8] []
+initialState = S 0 [[C 0 0]] [C 0 8] [] (randoms $ mkStdGen 0)
+mkInitialState blocks = S 0 blocks [C 0 8] [] (randoms $ mkStdGen 0)
 
 handleTime :: Double -> State -> State
 
 -- handleTime _ s = moveFalling D s
-handleTime double (S time fallingList falling static) = 
-  if (double > time) then S double fallingList (map (move D) falling) static 
-    else S time fallingList falling static
+handleTime double (S time fallingList falling static rands) = 
+  if (double > time) then S double fallingList (map (move D) falling) static rands
+    else S time fallingList falling static rands
 
 handleEvent :: Event -> State -> State
 handleEvent (KeyPress key) s
@@ -36,10 +38,10 @@ move R (C x y) = C (x + 1) y
 move D (C x y) = C x (y - 1)
 
 moveFalling :: Direction -> State -> State
-moveFalling d (S time fallingList falling static) = (S time fallingList (map (move d) falling) static)
+moveFalling d (S time fallingList falling static rands) = (S time fallingList (map (move d) falling) static rands)
 
 drawState :: State -> Picture
-drawState (S _ _ falling static) = pictureOfBoard falling static
+drawState (S _ _ falling static _) = pictureOfBoard falling static
 
 data Coord = C Integer Integer
 
@@ -58,6 +60,13 @@ fallingBoxes = [(C 1 1), (C 0 0)]
 
 staticBoxes :: [Coord]
 staticBoxes = [(C 2 2), (C 0 2)]
+
+--generates the next block randomly, and updates state accordingly
+generateBlock :: State -> (State, [Coord])
+generateBlock (S time fallingList falling static rands) = 
+    let block = fallingList !! (fromIntegral $ head $ rands) in
+    let state = S time fallingList falling static (tail rands) in
+    (state, block)
 
 --drawing
 wall, box :: Picture
