1bbf935d44626637688adafd524574f39fcd771b
Jess <yingjiao@sas.upenn.edu>
2016-11-30 23:27:30 -0500

    remove row works but something else is weird

--- tetris.hs
+++ tetris.hs
@@ -17,7 +17,7 @@ data State = S Double [[Coord]] [Coord] [Coord] [Integer]
 --main = interactionOf initialState handleTime handleEvent drawState
 
 -- initialState = S 0 [[C 0 0]] [C 0 8] [C 6 6, C 5 5, C 0 0] (randoms $ mkStdGen 0)
-mkInitialState blocks = S 0 blocks [C 0 8] wallCoord (randoms $ mkStdGen 0)
+mkInitialState blocks = S 0 blocks [C 0 8] [] (randoms $ mkStdGen 0)
 
 --all the wallCoordinates
 wallCoord :: [Coord]
@@ -26,33 +26,64 @@ wallCoord = add [] (-9)
         add list n = C 9 n : C n 9 : C n (-9) : C (-9) n : add list (n+1)
 
 handleTime :: Double -> State -> State
-handleTime _ (S 10 fallingList falling static _) = 
-  if canMove falling static D
+handleTime _ (S 3 fallingList falling static _) = 
+  if canMove (map (move D) falling) static D
     then S 0 fallingList (map (move D) falling) static []
-    else S 0 fallingList [C 0 8] (falling ++ static) []
-handleTime _ (S n fallingList falling static _) = S (n+1) fallingList (if canMove falling static D then (map (move D) falling) else falling) static []
+    else S 0 fallingList [C 0 8] (removeRow falling (falling ++ static)) []
+handleTime _ (S n fallingList falling static _) = S (n+1) fallingList falling static []
+
+
+move :: Direction -> Coord -> Coord
+move U (C x y) = C x (y + 1)
+move L (C x y) = C (x - 1) y
+move R (C x y) = C (x + 1) y
+move D (C x y) = C x (y - 1)
+
+
+handleEvent :: Event -> State -> State
+handleEvent (KeyPress key) s
+    | key == "Right" = moveFalling R s
+    | key == "Left"    = moveFalling L s
+handleEvent _ c = c
+
+moveFalling :: Direction -> State -> State
+moveFalling d (S time fallingList falling static rands) = (S time fallingList (moveCoords falling static d) static rands)
+
+-- moveFalling d (S time fallingList falling static rands) = (S time fallingList (map (move d) falling) static rands)
 
 -- takes a list of falling and static blocks and check if the falling blocks can move in the Direction anymore
+moveCoords :: [Coord] -> [Coord] -> Direction -> [Coord]
+moveCoords falling static direction = if canMove newFalling static direction then newFalling else falling
+-- moveCoords falling static direction = newFalling
+  where newFalling = map (move direction) falling
+
 canMove :: [Coord] -> [Coord] -> Direction -> Bool
-canMove falling static direction = intersect newfalling (static ++ wallCoord) == []
-  where newfalling = map (move direction) falling
+canMove falling static direction = intersect falling (static ++ wallCoord) == []
 
 -- takes falling coords and (falling + static) coords and check if a new row has been formed
 removeRow :: [Coord] -> [Coord] -> [Coord]
-removeRow falling static = falling
-  where getYCoord :: [Integer]
-        getYCoord = sort (nub (map (\(C _ y) -> y) falling)) -- get the Y coord of all the falling blocks in sorted order
-        
+removeRow falling static = helper getYCoord static --newStatic--helper2 newStatic newStatic
 
+  where newStatic = helper getYCoord static
+        getYCoord :: [Integer]
+        getYCoord = nub (map (\(C _ y) -> y) falling) -- get the Y coord of all the falling blocks in sorted order
+        
+        helper [] static = static
+        helper (x:xs) static = helper (xs) (remove x static)
+
+
+        -- helper2 [] static = static
+        -- helper2 (x:xs) static = helper2 xs ((moveCoords [x] static D) ++ (delete x static))    --if moveCoords [x] static D then helper2 xs ((move D x) : delete x static) else helper2 xs static
 
         -- gets a row and removes it if full
         remove :: Integer -> [Coord] -> [Coord]
         remove row static = if isFull row static then filter (\(C _ x) -> x /= row) static else static
+        isFull :: Integer -> [Coord] -> Bool
+        isFull row rowCoord = and (map (\r -> elem (C r row) rowCoord) rowOfInt)
         -- getRow row = filter (\(C _ y) -> y == row) static -- check if the entire row is there
         -- isPresent :: Integer -> [Coord] -> Bool
         -- isPresent int rowCoord = elem int (map (\(C x _) -> x) rowCoord) -- checks if a single number is in the list
-        isFull :: Integer -> [Coord] -> Bool
-        isFull row rowCoord = and (map (\r -> elem (C r row) rowCoord) rowOfInt)
+        
         --and (map (\r -> elem r (map (\(C x _) -> x) rowCoord)) rowOfInt)
         -- check if entire row is full
 
@@ -65,20 +96,9 @@ removeRow falling static = falling
         go 9 list = 9 : list
         go n list = n : go (n + 1) list
 
-handleEvent :: Event -> State -> State
-handleEvent (KeyPress key) s
-    | key == "Right" = moveFalling R s
-    | key == "Left"    = moveFalling L s
-handleEvent _ c = c
-
-move :: Direction -> Coord -> Coord
-move U (C x y) = C x (y + 1)
-move L (C x y) = C (x - 1) y
-move R (C x y) = C (x + 1) y
-move D (C x y) = C x (y - 1)
-
-moveFalling :: Direction -> State -> State
-moveFalling d (S time fallingList falling static rands) = (S time fallingList (map (move d) falling) static rands)
+rowOfCoord int = go (-9) []
+  where go 10 list = list
+        go n list = (C n int) : (go (n+1) list)
 
 drawState :: State -> Picture
 drawState (S _ _ falling static _) = pictureOfBoard falling static
@@ -139,3 +159,6 @@ drawTileAt tile c = atCoord c (drawTile tile)
 
 atCoord :: Coord -> Picture -> Picture
 atCoord (C x y) pic = translated (fromIntegral x) (fromIntegral y) pic
+
+
+
